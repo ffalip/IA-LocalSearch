0b1bc27dfde63982e36e7cbdb4cbd15e803e2ad2
ffalip <ferriolfalip@gmail.com>
2024-03-19 10:38:35 +0100

    ProbLSGoalTest

---
+++ CercaLocal/src/ProbLSGoalTest.java
@@ -0,0 +1,10 @@
+/** Imports */
+import aima.search.framework.GoalTest;
+
+public class ProbLSGoalTest  implements GoalTest {
+
+    public boolean isGoalState(Object state)
+    {
+        return false;
+    }
+}
