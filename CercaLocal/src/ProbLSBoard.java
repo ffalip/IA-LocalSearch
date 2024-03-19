96157c97d03613441713b2b5a55778a791636391
ffalip <ferriolfalip@gmail.com>
2024-03-19 10:30:01 +0100

    ProbLSboard

---
+++ CercaLocal/src/ProbLSBoard.java
@@ -0,0 +1,20 @@
+/**
+ * Representació de l'estat del problema
+ */
+
+/** Imports */
+
+
+public class ProbLSBoard {
+    /** Atributs */
+
+    /** Constructora
+     * Genera una instància del problema
+     */
+    public ProbLSBoard() {
+
+    }
+
+    /** Operadors */
+
+}
