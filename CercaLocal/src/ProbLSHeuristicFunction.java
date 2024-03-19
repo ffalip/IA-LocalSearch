3c32efc55a75f3190427bb19b4cd6847644dee1a
marti <marti.puerta>
2024-03-19 10:41:23 +0100

    heuristic class

--- CercaLocal/src/ProbLSHeuristicFunction.java
+++ CercaLocal/src/ProbLSHeuristicFunction.java
@@ -1,6 +1,7 @@
 import aima.search.framework.HeuristicFunction;
 public class ProbLSHeuristicFunction implements HeuristicFunction {
     public double getHeuristicValue(Object state) {
+        ProbLSBoard board = (ProbLSBoard)state;
         double sum = 0;
         //Calcular sum
         return (sum);
