17f36410a73fa3435a15ca62a4127348470bc704
marti <marti.puerta>
2024-04-02 10:23:02 +0200

    xd

---
+++ CercaLocal/src/ProbLSHeuristicFunction.java
@@ -0,0 +1,15 @@
+
+package ;
+
+/* Imports */
+
+import aima.search.framework.HeuristicFunction;
+public class ProbLSHeuristicFunction implements HeuristicFunction {
+
+    public double getHeuristicValue(Object state) {
+        ProbLSBoard board = (ProbLSBoard)state;
+        double sum = 0;
+        //Calcular sum
+        return (sum);
+    }
+}
