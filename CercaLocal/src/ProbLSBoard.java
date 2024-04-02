09d94e5fcae8a2bb4fc18281cd6e0fa41c7bd5f4
ffalip <ferriolfalip@gmail.com>
2024-04-02 10:57:14 +0200

    cosetes ProbLSBoard

--- CercaLocal/src/ProbLSBoard.java
+++ CercaLocal/src/ProbLSBoard.java
@@ -8,12 +8,32 @@ import IA.DistFS.*;
 public class ProbLSBoard {
     /* Atributs */
 
+    //private
+
     /** Constructora
      * Genera una instància del problema
      */
-    public ProbLSBoard() {
+    public ProbLSBoard(int users, int maxReq, int numServers, int minReps, int seed)
+    {
+        Requests req = new Requests(users, maxReq, seed);
+        try {
+            Servers serves = new Servers(numServers, minReps, seed);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        //inicialitzar matrius
+
 
     }
+
+    public int num_usuaris() {
+        return 0;
+    }
+
+    //public llista_usuaris()
+    //public llista_servers()
+
 
     /* Operadors */
 
