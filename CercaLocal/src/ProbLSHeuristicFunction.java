6c215d8dce2bbccdc396010ea6291a8a99a5acbd
marti <marti.puerta>
2024-04-02 10:57:00 +0200

    començar heuristica

--- CercaLocal/src/ProbLSHeuristicFunction.java
+++ CercaLocal/src/ProbLSHeuristicFunction.java
@@ -1,6 +1,3 @@
-
-package ;
-
 /* Imports */
 
 import aima.search.framework.HeuristicFunction;
@@ -8,8 +5,14 @@ public class ProbLSHeuristicFunction implements HeuristicFunction {
 
     public double getHeuristicValue(Object state) {
         ProbLSBoard board = (ProbLSBoard)state;
-        double sum = 0;
-        //Calcular sum
+        double sum = 0, nu;
+        nu = 10; // cambiar per nombre usuraris de state
+        for (int i = 0; i < nu; ++i) {
+            usuari =  board.getUsuari(i);
+            for (int j = 0; j < usuari.size(); ++j) {
+                sum += usuari[j].getTemps();
+            }
+        }
         return (sum);
     }
 }
