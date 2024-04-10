c19a8a50e6e2a9400ef826df795c2be3afc1886b
XaviMedinaFib <150521448+XaviMedinaFib@users.noreply.github.com>
2024-04-10 19:53:31 +0200

    Add files via upload

--- CercaLocal/src/ProbLSSuccessorFunction.java
+++ CercaLocal/src/ProbLSSuccessorFunction.java
@@ -19,19 +19,19 @@ public class ProbLSSuccessorFunction implements SuccessorFunction {
 
 
         for (int num_user = 0; num_user < pare.getNumUsers(); ++num_user){ // per cada usuari
-            for (int pos_fitxer = 0; pos_fitxer < pare.getUsuaris().get(num_user).length(); ++pos_fitxer){ // per cada fitxer de l'usuari
-                for (int num_server = 0; num_server < pare.num_servers(); ++num_server){ // per cada servidor
+            for (int pos_fitxer = 0; pos_fitxer < pare.getActualBoard().get(num_user).length(); ++pos_fitxer){ // per cada fitxer de l'usuari
+                for (int num_server = 0; num_server < pare.getNumServers(); ++num_server){ // per cada servidor
 
-                    int num_fitxer = pare.getUsuaris().get(num_user).get(pos_fitxer).getFirst();
+                    int num_fitxer = pare.getActualBoard().get(num_user).get(pos_fitxer).getFirst();
 
-                    if (pare.validFileServer(num_fitxer, num_server) && num_server != pare.getUsuaris().get(num_user).get(pos_fitxer).getSecond()){ // server es compatible amb el fitxer i no es el mateix server que ja tenia
+                    if (pare.validFileServer(num_fitxer, num_server) && num_server != pare.getActualBoard().get(num_user).get(pos_fitxer).getSecond()){ // server es compatible amb el fitxer i no es el mateix server que ja tenia
 
                         ProbLSBoard fill = new ProbLSBoard(pare.getNumServers(), pare.getNumUsers(), pare.getFileLoc(), pare.getTransTime(), pare.getActualBoard());
 
                         fill.changeTransmittingServer(num_user, num_fitxer, num_server);
 
                         double valor = LSHF.getHeuristicValue(fill);
-                        String string_info = "Fitxer " + num_fitxer + " del usuari " + num_user + " passa del servidor " + pare.getUsuaris().get(num_user).get(pos_fitxer).getSecond() + " al servidor " + num_server;
+                        String string_info = "Fitxer " + num_fitxer + " del usuari " + num_user + " passa del servidor " + pare.getActualBoard().get(num_user).get(pos_fitxer).getSecond() + " al servidor " + num_server;
 
                         // afegeix un fill al valor de return
                         retVal.add(new Successor(string_info, fill));
