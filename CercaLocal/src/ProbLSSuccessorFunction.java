1c48cb11dc2246398f8b749ae6a5b2ec6ebd4190
XaviMedinaFib <150521448+XaviMedinaFib@users.noreply.github.com>
2024-04-10 18:38:10 +0200

    Add files via upload

--- CercaLocal/src/ProbLSSuccessorFunction.java
+++ CercaLocal/src/ProbLSSuccessorFunction.java
@@ -14,27 +14,30 @@ public class ProbLSSuccessorFunction implements SuccessorFunction {
     public List getSuccessors(Object aState) { // retorna ArrayList on cada element es un sucessor
         ArrayList retVal = new ArrayList();
         ProbLSBoard pare = (ProbLSBoard) aState;
+        ProbLSHeuristicFunction LSHF  = new ProbLSHeuristicFunction();
 
-        for (int i = 0; i < 1; ++i){
 
-            // crear fill
-            ProbLSBoard fill = new ProbLSBoard("INFO PER CREAR EL FILL IGUAL AL PARE");
+        for (int num_user = 0; num_user < pare.num_usuaris(); ++num_user){ // per cada usuari
+            for (int num_fitxer = 0; num_fitxer < pare.getUsuaris()[nom_user].length(); ++num_fitxer){ // per cada fitxer de l'usuari
+                for (int num_server = 0; num_server < pare.num_servers(); ++num_server){ // per cada servidor
 
-            for (int num_user = 0; num_user < pare.num_usuaris(); ++num_user){
-                for (int num_fitxer = 0; num_fitxer < pare.llista_usuaris()[nom_user].length(); ++num_fitxer){
-                    for (int num_server = 0; num_server < pare.llista_servers().length(); ++num_server){
+                    if (pare.fitxerMesServerCompatibles(num_fitxer, num_server) && num_server != pare.getUsuaris()[num_user][num_fitxer].get_second()){ // server es compatible amb el fitxer i no es el mateix server que ja tenia
 
-                        if (pare.fitxer_i_server_compatibles(num_fitxer, num_server)){
-                            String string_info = "";
-                            // afegeix un fill al valor de return
-                            retVal.add(new Successor(string_info, fill));
-                        }
+                        ProbLSBoard fill = new ProbLSBoard("INFO PER CREAR EL FILL IGUAL AL PARE");
 
+                        fill.canviarFitxerDeServer(num_user, num_fitxer, num_server);
+
+                        double valor = LSHF.getHeuristicValue(fill);
+                        String string_info = "";
+
+                        // afegeix un fill al valor de return
+                        retVal.add(new Successor(string_info, fill));
                     }
+
                 }
             }
-
         }
+
 
         return retVal;
 
