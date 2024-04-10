2c1f63225f94ff4c974969b2e82f0ad0b09d2d7e
marti <marti.puerta>
2024-04-10 17:41:42 +0200

    heuristic be

--- CercaLocal/src/ProbLSHeuristicFunction.java
+++ CercaLocal/src/ProbLSHeuristicFunction.java
@@ -1,20 +1,31 @@
 /* Imports */
 
+import IA.DistFS.*;
 import aima.search.framework.HeuristicFunction;
-
 import java.util.ArrayList;
 
 public class ProbLSHeuristicFunction implements HeuristicFunction {
 
     public double getHeuristicValue(Object state) {
         ProbLSBoard board = (ProbLSBoard)state;
-        Integer sum = 0, nu;
+        Servers servers = board.getServers();
+
+        int nservers = servers.size();
+        int[] count_servidors = new int[nservers];
+
         ArrayList<ArrayList<Pair<Integer, Integer>>> usuaris = board.getUsuaris();
-        nu = usuaris.size(); // cambiar per nombre usuraris de state
+
+        int nu = usuaris.size(); // cambiar per nombre usuraris de state
         for (int i = 0; i < nu; ++i) {
             for (int j = 0; j < usuaris.get(i).size(); ++j) {
-                sum += usuaris.get(i).get(j).getSecond() * usuaris.get(i).get(j).getSecond();
+                int idServer = usuaris.get(i).get(j).getSecond();
+                count_servidors[idServer] += servers.tranmissionTime(i, idServer);
             }
+        }
+
+        int sum = 0;
+        for (int i = 0; i < nservers; ++i) {
+            sum += count_servidors[i]*count_servidors[i];
         }
         return (sum);
     }
