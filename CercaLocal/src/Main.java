3c42ae7ca29c0c2dfe1cb42d254b3c1533954cf0
marti <marti.puerta>
2024-04-11 11:15:46 +0200

    main

--- CercaLocal/src/Main.java
+++ CercaLocal/src/Main.java
@@ -1,5 +1,14 @@
 import java.io.IOException;
 import java.util.Random;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Properties;
+
+import aima.search.framework.Problem;
+import aima.search.framework.Search;
+import aima.search.framework.SearchAgent;
+import aima.search.informed.HillClimbingSearch;
+import aima.search.informed.SimulatedAnnealingSearch;
 
 
 public class Main {
@@ -25,6 +34,7 @@ public class Main {
             ProbLSBoard board = new ProbLSBoard(numUsers, maxReq, numServers, minReps, problemSeed);
             board.printState();
             board.printTransTime();
+            LSHillClimbingSearch(board);
         }
         else if (mode == 'M'){
             System.out.print("Introdueix el nombre de d'usuaris: ");
@@ -34,5 +44,36 @@ public class Main {
         }
         else System.out.println("El mode: " + mode + " no és vàlid");
 
+    }
+
+    private static void LSHillClimbingSearch(ProbLSBoard board) {
+        System.out.println("\nLS HillClimbing  -->");
+        try {
+            Problem problem =  new Problem(board,new ProbLSSuccessorFunction(), new ProbLSGoalTest(),new ProbLSHeuristicFunction());
+            Search search =  new HillClimbingSearch();
+            SearchAgent agent = new SearchAgent(problem,search);
+
+            System.out.println();
+            printActions(agent.getActions());
+            printInstrumentation(agent.getInstrumentation());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    private static void printInstrumentation(Properties properties) {
+        Iterator keys = properties.keySet().iterator();
+        while (keys.hasNext()) {
+            String key = (String) keys.next();
+            String property = properties.getProperty(key);
+            System.out.println(key + " : " + property);
+        }
+
+    }
+
+    private static void printActions(List actions) {
+        for (int i = 0; i < actions.size(); i++) {
+            String action = (String) actions.get(i);
+            System.out.println(action);
+        }
     }
 }
