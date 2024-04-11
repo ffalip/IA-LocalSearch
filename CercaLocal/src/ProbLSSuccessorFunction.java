b73a3cc86e462d1e7eabed123ade844a4d6e8abf
XaviMedinaFib <150521448+XaviMedinaFib@users.noreply.github.com>
2024-04-11 10:50:41 +0200

    Add files via upload

--- CercaLocal/src/ProbLSSuccessorFunction.java
+++ CercaLocal/src/ProbLSSuccessorFunction.java
@@ -17,21 +17,24 @@ public class ProbLSSuccessorFunction implements SuccessorFunction {
         ProbLSBoard pare = (ProbLSBoard) aState;
         ProbLSHeuristicFunction LSHF  = new ProbLSHeuristicFunction();
 
+        ArrayList users = pare.getUsersId();
 
-        for (int num_user = 0; num_user < pare.getNumUsers(); ++num_user){ // per cada usuari
-            for (int pos_fitxer = 0; pos_fitxer < pare.getActualBoard().get(num_user).size(); ++pos_fitxer){ // per cada fitxer de l'usuari
+        for (int num_user = 0; num_user < users.length; ++num_user){ // per cada usuari
+            for (int pos_fitxer = 0; pos_fitxer < pare.getActualBoard().get(id_user).length; ++pos_fitxer){ // per cada fitxer de l'usuari
                 for (int num_server = 0; num_server < pare.getNumServers(); ++num_server){ // per cada servidor
 
-                    int num_fitxer = pare.getActualBoard().get(num_user).get(pos_fitxer).getFirst();
+                    int id_user = users.get(num_user);
 
-                    if (pare.validFileServer(num_fitxer, num_server) && num_server != pare.getActualBoard().get(num_user).get(pos_fitxer).getSecond()){ // server es compatible amb el fitxer i no es el mateix server que ja tenia
+                    int num_fitxer = pare.getActualBoard().get(id_user).get(pos_fitxer).getFirst();
+
+                    if (pare.validFileServer(num_fitxer, num_server) && num_server != pare.getActualBoard().get(id_user).get(pos_fitxer).getSecond()){ // server es compatible amb el fitxer i no es el mateix server que ja tenia
 
                         ProbLSBoard fill = new ProbLSBoard(pare.getNumServers(), pare.getNumUsers(), pare.getFileLoc(), pare.getTransTime(), pare.getActualBoard());
 
-                        fill.changeTransmittingServer(num_user, num_fitxer, num_server);
+                        fill.changeTransmittingServer(id_user, num_fitxer, num_server);
 
                         double valor = LSHF.getHeuristicValue(fill);
-                        String string_info = "Fitxer " + num_fitxer + " del usuari " + num_user + " passa del servidor " + pare.getActualBoard().get(num_user).get(pos_fitxer).getSecond() + " al servidor " + num_server;
+                        String string_info = "Fitxer " + num_fitxer + " del usuari " + id_user + " passa del servidor " + pare.getActualBoard().get(id_user).get(pos_fitxer).getSecond() + " al servidor " + num_server;
 
                         // afegeix un fill al valor de return
                         retVal.add(new Successor(string_info, fill));
--- CercaLocal/src/ProbLSSuccessorFunctionSA.java
+++ CercaLocal/src/ProbLSSuccessorFunctionSA.java
@@ -19,25 +19,29 @@ public class ProbLSSuccessorFunctionSA implements SuccessorFunction {
         ProbLSHeuristicFunction LSHF  = new ProbLSHeuristicFunction();
         Random myRandom=new Random();
 
+        ArrayList users = pare.getUsersId();
+
         int num_user, pos_fitxer, num_server;
 
-        num_user = myRandom.nextInt(pare.getNumUsers());
+        num_user = myRandom.nextInt(users.length);
 
-        pos_fitxer = myRandom.nextInt(pare.getActualBoard().get(num_user).size());
+        int id_user = users.get(num_user);
 
-        int num_fitxer = pare.getActualBoard().get(num_user).get(pos_fitxer).getFirst();
+        pos_fitxer = myRandom.nextInt(pare.getActualBoard().get(id_user).length);
+
+        int num_fitxer = pare.getActualBoard().get(id_user).get(pos_fitxer).getFirst();
 
         do{
             num_server = myRandom.nextInt(pare.getNumServers());
-        } while( !(pare.validFileServer(num_fitxer, num_server) && num_server != pare.getActualBoard().get(num_user).get(pos_fitxer).getSecond()) ); // mentres el server no sigui possible buscar un nou
+        } while( !(pare.validFileServer(num_fitxer, num_server) && num_server != pare.getActualBoard().get(id_user).get(pos_fitxer).getSecond()) ); // mentres el server no sigui possible buscar un nou
 
 
         ProbLSBoard fill = new ProbLSBoard(pare.getNumServers(), pare.getNumUsers(), pare.getFileLoc(), pare.getTransTime(), pare.getActualBoard());
 
-        fill.changeTransmittingServer(num_user, num_fitxer, num_server);
+        fill.changeTransmittingServer(id_user, num_fitxer, num_server);
 
         double valor = LSHF.getHeuristicValue(fill);
-        String string_info = "Fitxer " + num_fitxer + " del usuari " + num_user + " passa del servidor " + pare.getActualBoard().get(num_user).get(pos_fitxer).getSecond() + " al servidor " + num_server;
+        String string_info = "Fitxer " + num_fitxer + " del usuari " + id_user + " passa del servidor " + pare.getActualBoard().get(id_user).get(pos_fitxer).getSecond() + " al servidor " + num_server;
 
         // afegeix un fill al valor de return
         retVal.add(new Successor(string_info, fill));
